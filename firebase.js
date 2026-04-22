db8e56de6f6ced280ce5925db444860bc80a2eaa
MD DULAL HOSAN <42477795+mddulal596@users.noreply.github.com>
2026-04-22 21:21:25 +0600

    Downgrade Firebase SDK to version 10.7.1
    Updated Firebase SDK imports to version 10.7.1 and modified app export.

--- firebase.js
+++ firebase.js
@@ -1,14 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA-Oa4ZWgGA3TrQFRUrEz3Y8CXkYd4hmyM",
   authDomain: "microjobs-13730.firebaseapp.com",
   databaseURL: "https://microjobs-13730-default-rtdb.firebaseio.com",
   projectId: "microjobs-13730",
+  storageBucket: "microjobs-13730.firebasestorage.app",
+  messagingSenderId: "288652584372",
+  appId: "1:288652584372:web:331a3867f481ffe91bfee1",
+  measurementId: "G-VXYSNKCSYD"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
