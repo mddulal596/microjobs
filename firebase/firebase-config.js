d35866f35f4675690f7f1af73a74e359a4142b46
MD DULAL HOSAN <42477795+mddulal596@users.noreply.github.com>
2026-07-22 18:08:44 +0600

    Add Firebase configuration imports

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,3 +1,4 @@
+// firebase/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
